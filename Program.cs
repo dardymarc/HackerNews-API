63fc2281b239898bef36837f03839ce734118f0c
dardymarc <61477183+dardymarc@users.noreply.github.com>
2026-04-23 18:35:53 +0000

    Delete

--- Program.cs
+++
@@ -1,9 +0,0 @@
-builder.Services.AddMemoryCache();
-
-builder.Services.AddScoped<StoryService>();
-builder.Services.AddScoped<IStoryService>(provider =>
-{
-    var inner = provider.GetRequiredService<StoryService>();
-    var cache = provider.GetRequiredService<IMemoryCache>();
-    return new CachedStoryService(inner, cache);
-});
