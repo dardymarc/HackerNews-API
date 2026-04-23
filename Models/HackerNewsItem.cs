780c4922de8c804a85380ee1e11bdaaf6493a22c
dardymarc <61477183+dardymarc@users.noreply.github.com>
2026-04-23 11:53:36 -0400

    Create HackerNewsItem.cs

---
+++ Models/HackerNewsItem.cs
@@ -0,0 +1,10 @@
+public class HackerNewsItem
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Url { get; set; }
+    public string? By { get; set; }
+    public long Time { get; set; }
+    public int Score { get; set; }
+    public string? Type { get; set; }
+}
